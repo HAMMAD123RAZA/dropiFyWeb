4d1f32117ae77644bccd139e1fd121969d91bc2e
Hammad Raza <01hammadraza@gmail.com>
2024-12-31 00:31:59 +0500

    here

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,19 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: '*', // Allow all origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  }));
+
+app.use(
+  cors({
+    origin:'*', 
+    credentials:true,           
+    optionSuccessStatus:200
+  })
+);
+
+// app.use(cors({
+//     origin: '*',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   }));
     
 app.use(express.urlencoded({ extended: true }));
 
