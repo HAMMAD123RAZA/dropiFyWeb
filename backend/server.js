f59ed741ad3b62f91c7a18c86294ee1398fd8699
Hammad Raza <01hammadraza@gmail.com>
2024-12-31 01:11:40 +0500

    here

--- backend/server.js
+++ backend/server.js
@@ -23,13 +23,13 @@ const app = express();
 app.use(express.json());
 
 app.use(
-  cors({
-    origin:'*', 
-    credentials:true,           
-    optionSuccessStatus:200
-  })
-);
-
+    cors({
+      origin: 'http://localhost:5173', // Allow only this specific origin
+      credentials: true,              // Enable credentials for this origin
+      optionSuccessStatus: 200        // Response status for successful options request
+    })
+  )
+  
 // app.use(cors({
 //     origin: '*',
 //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
